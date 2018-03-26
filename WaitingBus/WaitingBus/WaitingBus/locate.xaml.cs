7e227021a4f54e91816cee856fbcd627f0509a79
linhs <linhs1109@gmail.com>
2018-03-26 20:48:27 +0800

    init

---
+++ WaitingBus/WaitingBus/WaitingBus/locate.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WaitingBus
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class locate : ContentPage
+	{
+		public locate ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
